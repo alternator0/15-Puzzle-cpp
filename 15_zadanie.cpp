8acf24ade41a42fb97c964e08fd015cce5d8b938
alternator0 <dziadzius88@protonmail.com>
2024-02-22 17:21:58 +0100

    .

---
+++ 15_zadanie.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include "add.h"
+
+int main()
+{
+    std::cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n';
+//int getValueFromUser() {
+//    std::cout << "Enter an integer: ";
+//    int input{};
+//    std::cin >> input;
+//
+//    return input;
+//}
+//
+//int doubleNumber(int x)
+//{
+//   return x * 2;
+//}
+//
+//int main() {
+//    int number{getValueFromUser()};
+//    int result {doubleNumber(number)};
+//    std::cout << number << " * 2 = " << result;
+    return 0;
+}
